fcb53d348fd0c2232c42b2fdebc0ec3e7ede07b7
charankamal20 <charanksingh20@gmail.com>
2024-06-28 22:46:58 +0530

    Integrate Mongo DB

---
+++ initializers/connectToDb.go
@@ -0,0 +1,47 @@
+package initializers
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var UserCollection *mongo.Collection
+var MongoClient *mongo.Client
+
+func ConnectToDB() {
+
+	uri := os.Getenv("MONGODB_URI")
+
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+  // Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+  serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+  opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+  // Create a new client and connect to the server
+  client, err := mongo.Connect(context.TODO(), opts)
+  MongoClient = client
+
+  if err != nil {
+    panic(err)
+  }
+  
+  // Send a ping to confirm a successful connection
+  if err := client.Database("admin").RunCommand(context.TODO(), bson.M{"ping": 1}).Err(); err != nil {
+    panic(err)
+  }
+
+  UserCollection = client.Database("Users").Collection("user_data")
+
+  fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
---
+++ middleware/auth.go
@@ -0,0 +1,54 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+
+
+func RequireAuth(context *gin.Context) {
+	// Get Cookie
+	tokenString, err := context.Cookie("Authorization")
+
+	if err != nil {
+		context.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	// Decode/ validate
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("JWT_KEY")), nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		context.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if float64(time.Now().Unix()) > claims["exp"].(float64) {
+		context.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	// !TODO: Check is user exists
+
+	context.Set("email", claims["email"])
+
+	context.Next()
+}
